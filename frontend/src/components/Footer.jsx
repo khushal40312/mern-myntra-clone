162c02d7f8bca9f6d9117d8d0272b5cac9591395
khushal <khushalsharma40312@gmail.com>
2024-09-23 14:21:01 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
@@ -6,33 +7,33 @@ export default function Footer() {
       <div className="footer_container">
         <div className="footer_column">
           <h3>Shop By Category</h3>
-          <a href="#">Men's Clothing</a>
-          <a href="#">Women's Clothing</a>
-          <a href="#">Kids' Clothing</a>
-          <a href="#">Home & Living</a>
-          <a href="#">Beauty Products</a>
-          <a href="#">Gift Cards</a>
-          <a href="#">Loyalty Programs</a>
+          <Link to="/men">Men's Clothing</Link>
+          <Link to="/women">Women's Clothing</Link>
+          <Link to="kids">Kids' Clothing</Link>
+          <Link to="/homeliving">Home & Living</Link>
+          <Link to="/beauty">Beauty Products</Link>
+          <Link to="/giftcard">Gift Cards</Link>
+
         </div>
 
         <div className="footer_column">
           <h3>Customer Service</h3>
-          <a href="#">Help & Support</a>
-          <a href="#">Return Policy</a>
-          <a href="#">Order Tracking</a>
-          <a href="#">Contact Us</a>
-          <a href="#">FAQs</a>
-          <a href="#">Shipping Information</a>
+          <Link to="/helpandsupport">Help & Support</Link>
+          <Link to="/returnpolicy">Return Policy</Link>
+          <Link to="/orders">Order Tracking</Link>
+          <Link to="/helpandsupport">Contact Us</Link>
+          <Link to="/faq">FAQs</Link>
+          <Link to="/orders">Shipping Information</Link>
         </div>
 
         <div className="footer_column">
           <h3>About Us</h3>
-          <a href="#">Our Story</a>
-          <a href="#">Careers</a>
-          <a href="#">Press</a>
-          <a href="#">Investor Relations</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Terms of Service</a>
+          <Link to="/ourstory">Our Story</Link>
+          <Link to="/careers">Careers</Link>
+          <Link to="/press">Press</Link>
+          <Link to="/investorrelations">Investor Relations</Link>
+          <Link to="/privacypolicy">Privacy Policy</Link>
+          <Link to="/tc">Terms of Service</Link>
         </div>
       </div>
       <hr />
