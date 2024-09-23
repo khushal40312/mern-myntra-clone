52d8c81a0b5660af276f344339d97600ad5cb1a2
khushal <khushalsharma40312@gmail.com>
2024-09-23 15:20:24 +0530

    Update Login.jsx

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -17,7 +17,7 @@ export default function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const response = await fetch("http://localhost:8009/api/auth/login", {
+        const response = await fetch("https://myntra-clone-mern.onrender.com/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
