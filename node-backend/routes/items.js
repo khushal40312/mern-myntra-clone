1717dc56c7fd62999dbd73bd4ae4d3c43e18cf16
khushal <khushalsharma40312@gmail.com>
2024-10-19 16:53:21 +0530

    Update items.js

--- node-backend/routes/items.js
+++ node-backend/routes/items.js
@@ -79,12 +79,11 @@ router.delete('/deleteitem/:id', fetchuser, async (req, res) => {
     if (!cart) {
       return res.status(404).send("Item not found");
     }
-console.log(req.user.id)
-    console.log(cart.user.id)
+
     // Allow deletion only if the user owns this item
-    if (cart.user.toString() !== req.user.id) {
-      return res.status(401).send("Not Allowed");
-    }
+if (!cart.user.equals(mongoose.Types.ObjectId(req.user.id))) {
+  return res.status(401).send("Not Allowed");
+}
 
     // Delete the item
     await CartID.deleteOne({ id: req.params.id });
